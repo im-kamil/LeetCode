f36dca62fc3517bb0fe33005021e9b6460bcac08
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-05-28 19:37:57 +0530

    Time: 1 ms (65.97%), Space: 51.3 MB (36.97%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for(int num: nums)
+            sum += num;
+            
+        return (nums.length * (nums.length + 1) )/ 2 - sum;
+    }
+}
