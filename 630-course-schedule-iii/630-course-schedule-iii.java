5c018786f1ae7858ee69c7b761f04232f4a0606a
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-06-23 16:39:16 +0530

    Time: 79 ms (20.56%), Space: 65.8 MB (78.52%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -25,3 +25,18 @@ class Solution {
         return pq.size();
     }
 }
+//    in short 
+//   Arrays.sort(courses, (a,b)-> a[1]==b[1] ? a[0]-b[0] : a[1]-b[1]);
+//         PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->b-a);
+//         int time=0;
+//         for (int[] course : courses) 
+//         {
+// 			//Add current course to a priority queue
+//             time+=course[0]; 
+//             pq.offer(course[0]);
+// 			//If time exceeds, drop the previous course which costs the most time.
+//             if (time>course[1]) time-=pq.poll(); 
+//         }        
+//         return pq.size();
+//     }
+// }
