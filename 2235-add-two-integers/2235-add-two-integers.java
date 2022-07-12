fb7b5bee10df12cbc6de734e38917168c1a081d4
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-07-12 13:07:14 +0530

    Time: 4 ms (65.48%), Space: 40.6 MB (75.06%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int sum(int num1, int num2) {
+        // int num1 = 12;
+        // int num2 = 17;
+        int sum = num1+num2;
+        System.out.println(sum);
+        return sum;
+    }
+}
