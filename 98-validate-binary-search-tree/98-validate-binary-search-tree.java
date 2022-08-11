75f9bbb4c172b95c51fb1e9000528333b942c4c9
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-08-11 17:32:54 +0530

    Time: 1 ms (62.14%), Space: 43.5 MB (78.09%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+         return helper(root, null, null);
+    }
+    
+    boolean helper(TreeNode root, Integer min, Integer max) {
+        if (root == null)
+            return true;
+        
+        if ((min != null && root.val <= min) || (max != null && root.val >= max))
+            return false;
+        
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    
+    }
+}
