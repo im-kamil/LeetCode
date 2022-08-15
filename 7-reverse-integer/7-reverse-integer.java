de98de84dc015afc483278f73ed594753dc5521c
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-08-15 10:43:15 +0530

    Time: 3 ms (31.40%), Space: 40.9 MB (79.09%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+         int prevRev = 0 , rev= 0;
+        while( x != 0){
+            rev= rev*10 + x % 10;
+            if((rev - x % 10) / 10 != prevRev){
+                return 0;
+            }
+            prevRev = rev;
+            x= x/10;
+        }
+        return rev;
+    }
+}
