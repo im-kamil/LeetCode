52ddb3de52c137eb77e68f37103db3e5a1018ee8
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-09-09 12:02:33 +0530

    Time: 195 ms (36.25%), Space: 129.9 MB (69.77%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfWeakCharacters(int[][] properties) {
+        // sorting 
+        int n = properties.length;
+        int count = 0;
+        
+        Arrays.sort(properties, (a, b) -> (b[0] == a[0]) ? (a[1] - b[1]) : b[0] - a[0]);
+        
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            
+            if (properties[i][1] < max) {
+                count++;
+            }
+            max = Math.max(max, properties[i][1]);
+        }
+        return count;
+    }
+}
