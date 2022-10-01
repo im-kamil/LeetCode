48041c96233415ad64a0816240db97a915fa5eb3
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-10-02 01:05:34 +0530

    Time: 2 ms (81.28%), Space: 53.5 MB (19.25%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+         int total = 0, sum = 0;
+        for (int num : nums) total += num;
+        for (int i = 0; i < nums.length; sum += nums[i++])
+            if (sum * 2 == total - nums[i]) return i;
+        return -1; 
+        
+        
+    }
+}
