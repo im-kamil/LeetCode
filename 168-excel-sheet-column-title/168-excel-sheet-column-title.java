201907ce6fef8401105d4d0dc71c351d38641369
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-10-08 23:37:33 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (82.27%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String convertToTitle(int n) {
+         StringBuilder result = new StringBuilder();
+
+        while(n>0){
+            n--;
+            result.insert(0, (char)('A' + n % 26));
+            n /= 26;
+        }
+
+        return result.toString();
+    }
+}
