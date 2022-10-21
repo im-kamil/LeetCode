487343a79bc7651cab691de793db7b89429e9a3e
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-10-21 10:40:14 +0530

    Time: 78 ms (69.98%), Space: 83.9 MB (26.24%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+         if (nums == null || nums.length == 0 || k <= 0) {
+            return false;
+        }
+
+        final TreeSet<Integer> values = new TreeSet<>();
+        for (int ind = 0; ind < nums.length; ind++) {
+
+            final Integer floor = values.floor(nums[ind] + t);
+            final Integer ceil = values.ceiling(nums[ind] - t);
+            if ((floor != null && floor >= nums[ind])
+                    || (ceil != null && ceil <= nums[ind])) {
+                return true;
+            }
+
+            values.add(nums[ind]);
+            if (ind >= k) {
+                values.remove(nums[ind - k]);
+            }
+        }
+
+        return false;
+    }
+}
