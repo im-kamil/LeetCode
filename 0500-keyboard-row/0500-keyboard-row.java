e5d9efcdbbecc746afe57f8ff27d40dbf298b1c2
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-10-21 10:40:47 +0530

    Time: 9 ms (6.77%), Space: 42.4 MB (13.99%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -0,0 +1,5 @@
+class Solution {
+    public String[] findWords(String[] words) {
+       return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")).toArray(String[]::new);
+}
+}
