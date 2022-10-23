e6b349a9846d0b88b7b6215ed2caed6e0517523e
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-10-23 09:36:49 +0530

    Time: 3 ms (86.64%), Space: 54.9 MB (30.32%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+         int[] res = new int[2];
+    for (int i : nums) {
+        if (nums[Math.abs(i) - 1] < 0) res[0] = Math.abs(i);
+	else nums[Math.abs(i) - 1] *= -1;
+    }
+    for (int i=0;i<nums.length;i++) {
+        if (nums[i] > 0) res[1] = i+1;
+    }
+    return res;
+    }
+}
