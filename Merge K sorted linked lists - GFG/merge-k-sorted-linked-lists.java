462c87203520eec5583f06958868020a0824a246
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2022-11-14 00:17:25 +0530

    Added solution - LeetHub

---
+++ Merge K sorted linked lists - GFG/merge-k-sorted-linked-lists.java
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+
+class GfG
+{
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {   
+            int N = sc.nextInt();
+            
+            Node []a = new Node[N];
+            
+            for(int i = 0; i < N; i++)
+            {
+                int n = sc.nextInt();
+                
+                Node head = new Node(sc.nextInt());
+                Node tail = head;
+                
+                for(int j=0; j<n-1; j++)
+                {
+                    tail.next = new Node(sc.nextInt());
+                    tail = tail.next;
+                }
+                
+                a[i] = head;
+            }
+            
+            Solution g = new Solution();
+             
+            Node res = g.mergeKList(a,N);
+            if(res!=null)
+                printList(res);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+// a is an array of Nodes of the heads of linked lists
+// and N is size of array a
+
+
+class Solution
+{
+    //Function to merge K sorted linked list.
+    Node mergeKList(Node[]arr,int K)
+    {
+        //Add your code here.
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+
+        for(int i=0;i<K;i++){
+
+            while(arr[i]!=null){
+
+                pq.add(arr[i].data);
+
+                arr[i]=arr[i].next;
+
+            }
+
+        }
+
+        Node head=new Node(-1);
+
+        Node tail=head;
+
+        while(pq.size()>0){
+
+            Node cur=new Node(pq.poll());
+
+            tail.next=cur;
+
+            tail=tail.next;
+
+        }
+
+       tail.next=null;
+
+        return head.next;
+    }
+}
