1f9d0086b1cb76c5d79079f4d33063c95215a3b4
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-02-27 07:16:27 +0530

    Added solution - LeetHub

---
+++ Reverse both parts - GFG/reverse-both-parts.java
@@ -0,0 +1,129 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+
+    public static Node inputList(BufferedReader br) throws IOException
+    {
+        int n = Integer.parseInt(br.readLine().trim()); // Length of Linked List
+
+        String[] s = br.readLine().trim().split(" ");
+        Node head = new Node(Integer.parseInt(s[0])), tail = head;
+        for(int i = 1; i < s.length; i++)
+            tail = tail.next = new Node(Integer.parseInt(s[i]));
+
+        return head;
+    }
+
+    public static void printList(Node node)
+    {
+        while (node != null)
+        {
+    		System.out.print(node.data + " ");
+    		node = node.next;
+    	}
+    	System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            Node head = Node.inputList(br);
+            
+            
+            int k;
+            k = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            Node res = obj.reverse(head, k);
+            
+            Node.printList(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for singly Link List Node
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+
+You can also use the following for printing the link list.
+Node.printList(Node node);
+*/
+
+class Solution {
+    public static Node reverse(Node head, int k) {
+        // code here
+         int i;
+
+        Node temp=head;
+
+        ArrayList<Integer> list=new ArrayList<>();
+
+        while(temp!=null) {
+
+            list.add(temp.data);
+
+            temp=temp.next;
+
+        }
+
+        i=k-1;
+
+        temp=head;
+
+        while(i>=0) {
+
+            temp.data=list.get(i);
+
+            i--;
+
+            temp=temp.next;
+
+        }
+
+        i=list.size()-1;
+
+        while(i>=k) {
+
+            temp.data=list.get(i);
+
+            i--;
+
+            temp=temp.next;
+
+        }
+
+        return head;
+    }
+}
+        
