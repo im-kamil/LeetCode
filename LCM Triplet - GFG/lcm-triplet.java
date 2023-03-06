088460e75ebeb9e15eaf37ef089a4c6aa1df38f2
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-03-07 01:55:16 +0530

    Added solution - LeetHub

---
+++ LCM Triplet - GFG/lcm-triplet.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            long N = Long.parseLong(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.lcmTriplets(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    long lcmTriplets(long N) {
+        // code here
+        if(N<3)
+
+         return N;
+
+         if(N%2!=0) return N*(N-1)*(N-2);
+
+         else
+
+            {
+
+                if(N%3==0) return (N-1)*(N-2)*(N-3);
+
+                else
+
+                return N*(N-1)*(N-3);
+    }
+}
+}
