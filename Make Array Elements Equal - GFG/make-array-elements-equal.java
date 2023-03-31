8fe1ce4b372d686a20edb7558e71f3f66c234078
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-04-01 03:36:18 +0530

    Added solution - LeetHub

---
+++ Make Array Elements Equal - GFG/make-array-elements-equal.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            int N = Integer.parseInt(br.readLine().trim());
+
+            Solution ob = new Solution();
+            System.out.println(ob.minOperations(N));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public long minOperations(int N) {
+        // Code here
+        if(N%2==0) return (long)(N/2)*(N/2);
+        return (long)(N/2)*(N/2+1);
+    }
+}
