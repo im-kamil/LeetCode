a7c4d520913c79b0b683a2e146df2f8f89ad8aff
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-04-02 00:52:26 +0530

    Added solution - LeetHub

---
+++ Prerequisite Tasks - GFG/prerequisite-tasks.java
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main(String args[]) throws IOException
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+		while(t>0)
+		{
+		    int N = sc.nextInt();
+		    int P = sc.nextInt();
+		    int prerequisites[][] = new int[P][2];
+		    for(int i=0;i<P;i++)
+		    {
+		        for(int j=0;j<2;j++)
+		        {
+		            prerequisites[i][j] = sc.nextInt();
+		        }
+		    }
+			Solution ob = new Solution();
+			if(ob.isPossible(N,prerequisites))
+			{
+			    System.out.println("Yes");
+			}
+			else{
+			    System.out.println("No");
+			}
+			t--;
+		}
+	}
+	
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public boolean isPossible(int V, int[][] prerequisites)
+    {
+        // Your Code goes here
+         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        int m = prerequisites.length;
+        for (int i = 0; i < m; i++) {
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+
+
+
+        int indegree[] = new int[V];
+        for (int i = 0; i < V; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+
+        Queue<Integer> q = new LinkedList<Integer>();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+
+        List<Integer> topo = new ArrayList<Integer>();
+        // o(v + e)
+        while (!q.isEmpty()) {
+            int node = q.peek();
+
+            q.remove();
+            topo.add(node);
+            // node is in your topo sort
+            // so please remove it from the indegree
+
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) q.add(it);
+            }
+        }
+
+
+        if (topo.size() == V) return true;
+        return false;
+    }
+    
+}
