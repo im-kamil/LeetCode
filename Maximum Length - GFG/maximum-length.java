98b39822cb4e807054f0de04e3d85cea7d498ee8
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-04-11 05:51:07 +0530

    Added solution - LeetHub

---
+++ Maximum Length - GFG/maximum-length.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class FastReader{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader(){ 
+        br = new BufferedReader(new InputStreamReader(System.in)); 
+    } 
+
+    String next(){ 
+        while (st == null || !st.hasMoreElements()){ 
+            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    String nextLine(){ 
+        String str = ""; 
+        try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+        return str; 
+    }
+    
+    Integer nextInt(){
+        return Integer.parseInt(next());
+    }
+} 
+    
+class GFG {
+    public static void main(String args[]) throws IOException {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+    
+            Solution ob = new Solution();
+            int ans = ob.solve(a, b, c);
+            
+            out.println(ans);
+        }
+        out.flush();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int solve(int a, int b, int c) {
+        // code here
+        int arr[]= {a,b,c};
+        Arrays.sort(arr);
+        int k = arr[0]+arr[1]+1;
+        k*=2;
+        if(k>=arr[2]){
+            return (a+b+c);
+        }
+        return -1;
+    }
+};
