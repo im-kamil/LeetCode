f0291b01f9a0f4dc23c9705a67ff9fde56705b3e
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-04-17 14:09:49 +0530

    Added solution - LeetHub

---
+++ Job Sequencing Problem - GFG/job-sequencing-problem.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
+            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+        // Your code here
+        boolean[] jobsDone = new boolean[n];
+        Arrays.fill(jobsDone, false);
+        Arrays.sort(arr, (a, b) -> b.profit - a.profit);
+        int cnt = 0, profit = 0;
+        for (Job job : arr) {
+            for (int i = Math.min(job.deadline - 1, n - 1); i >= 0; i--) {
+                if (!jobsDone[i]) {
+                    cnt++;
+                    profit += job.profit;
+                    jobsDone[i] = true;
+                    break;
+                }
+            }
+        }
+        return new int[]{cnt, profit};
+    }
+}
