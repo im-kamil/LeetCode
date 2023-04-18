54788e6f1d844eb08aef3059db3929711f52b726
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-04-18 14:55:37 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (54.27%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int length1 = word1.length(), length2 = word2.length();
+        
+        char[] resultArr = new char[length1 + length2];
+        
+        // Two pointers are maintained which will be used to 
+        // fetch the characters in alternating order.       
+        int pointer1 = 0, pointer2 = 0, resultPointer = 0;
+        
+        //When either of the word's traversal is completed then loop
+        //will only consider the other word.
+        while (pointer1 < length1 || pointer2 < length2) {
+            if (pointer1 < length1) {
+                resultArr[resultPointer++] = word1.charAt(pointer1++);
+            }
+            if (pointer2 < length2) {
+                resultArr[resultPointer++] = word2.charAt(pointer2++);
+            }
+        }
+        // return new String(resultArr);
+        return new String(resultArr);
+    }
+}
