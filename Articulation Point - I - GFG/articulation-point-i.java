4f21c9058fa3460c802a95f789490f147f3e8150
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-05-01 14:27:14 +0530

    Added solution - LeetHub

--- Articulation Point - I - GFG/articulation-point-i.java
+++ Articulation Point - I - GFG/articulation-point-i.java
@@ -39,58 +39,53 @@ class GFG
 class Solution
 {
     //Function to return Breadth First Traversal of given graph.
-    private int time =0;
-    public ArrayList<Integer> articulationPoints(int V,ArrayList<ArrayList<Integer>> adj)
-    {
-        // Code here
-        int disc[] = new int[V];
-        int low[] = new int[V];
-        boolean vis[] = new boolean[V];
-        int parent[] = new int[V];
-        boolean ap[] = new boolean[V];
-        ArrayList<Integer> list = new ArrayList<>();
-        Arrays.fill(parent, -1);
-        
-        for(int i=0; i<V; i++){
-            if(vis[i] == false){
-                ArticulationPoint(i, adj, disc, low, vis, parent, ap);
+    private int timer = 1;
+    private void dfs(int node, int parent, int[] vis,
+                     int tin[], int low[], int[] mark,
+                     ArrayList<ArrayList<Integer>> adj) {
+        vis[node] = 1;
+        tin[node] = low[node] = timer;
+        timer++;
+        int child = 0;
+        for (Integer it : adj.get(node)) {
+            if (it == parent) continue;
+            if (vis[it] == 0) {
+                dfs(it, node, vis, tin, low, mark, adj);
+                low[node] = Math.min(low[node], low[it]);
+                // node --- it
+                if (low[it] >= tin[node] && parent != -1) {
+                    mark[node] = 1;
+                }
+                child++;
+            } else {
+                low[node] = Math.min(low[node], tin[it]);
             }
         }
-        for(int i=0; i<V; i++){
-            if(ap[i] == true){
-                list.add(i);
-            }
+        if (child > 1 && parent == -1) {
+            mark[node] = 1;
         }
-        if(list.size() == 0){
-            list.add(-1);
-            return list;
-        }
-        Collections.sort(list);
-        return list;
     }
-    
-    private void ArticulationPoint(int u, ArrayList<ArrayList<Integer>> adj, int disc[], int low[],
-        boolean []vis, int parent[], boolean []ap){
-        vis[u] = true;
-        disc[u] = low[u] = time++;
-        int children = 0;
-        for(int v : adj.get(u)){
-            if(vis[v] == false){
-                children++;
-                parent[v] = u;
-                
-                ArticulationPoint(v, adj, disc, low, vis, parent, ap);
-                low[u] = Math.min(low[u], low[v]);
-                if(parent[u] == -1 && children > 1){
-                    ap[u] = true;
-                }
-                else if(parent[u] != -1 && low[v] >= disc[u]){
-                    ap[u] = true;
-                }
-            }
-            else{
-                low[u] = Math.min(low[u], disc[v]);
+    //Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> articulationPoints(int n,
+            ArrayList<ArrayList<Integer>> adj) {
+        int[] vis = new int[n];
+        int[] tin = new int[n];
+        int[] low = new int[n];
+        int[] mark = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == 0) {
+                dfs(i, -1, vis, tin, low, mark, adj);
             }
         }
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (mark[i] == 1) {
+                ans.add(i);
+            }
+        }
+        if (ans.size() == 0) {
+            ans.add(-1);
+        }
+        return ans;
     }
 }
