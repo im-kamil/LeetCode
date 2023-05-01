ef44dd12c55e4c61a218836ea4c11b828b17eae0
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-05-01 13:00:47 +0530

    Time: 104 ms (80.89%), Space: 108.8 MB (67.75%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,34 +1,41 @@
 class Solution {
-     int[] disc, low;
-    int time = 1;
-    List<List<Integer>> ans = new ArrayList<>();
-    Map<Integer,List<Integer>> edgeMap = new HashMap<>();
-    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        disc = new int[n];
-        low = new int[n];
-        for (int i = 0; i < n; i++)
-            edgeMap.put(i, new ArrayList<Integer>());
-        for (List<Integer> conn : connections) {
-            edgeMap.get(conn.get(0)).add(conn.get(1));
-            edgeMap.get(conn.get(1)).add(conn.get(0));
+     private int timer = 1;
+    private void dfs(int node, int parent, int[] vis,
+                     ArrayList<ArrayList<Integer>> adj, int tin[], int low[],
+                     List<List<Integer>> bridges) {
+        vis[node] = 1;
+        tin[node] = low[node] = timer;
+        timer++;
+        for (Integer it : adj.get(node)) {
+            if (it == parent) continue;
+            if (vis[it] == 0) {
+                dfs(it, node, vis, adj, tin, low, bridges);
+                low[node] = Math.min(low[node], low[it]);
+                // node --- it
+                if (low[it] > tin[node]) {
+                    bridges.add(Arrays.asList(it, node));
+                }
+            } else {
+                low[node] = Math.min(low[node], low[it]);
+            }
         }
-        dfs(0, -1);
-        return ans;
     }
-    public void dfs(int curr, int prev) {
-        disc[curr] = low[curr] = time++;
-        for (int next : edgeMap.get(curr)) {
-            if (disc[next] == 0) {
-                dfs(next, curr);
-                low[curr] = Math.min(low[curr], low[next]);
-            } else if (next != prev)
-                low[curr] = Math.min(low[curr], disc[next]);
-            if (low[next] > disc[curr]) 
-                ans.add(Arrays.asList(curr, next));
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        ArrayList<ArrayList<Integer>> adj =
+            new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<Integer>());
         }
+        for (List<Integer> it : connections) {
+            int u = it.get(0); int v = it.get(1);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        int[] vis = new int[n];
+        int[] tin = new int[n];
+        int[] low = new int[n];
+        List<List<Integer>> bridges = new ArrayList<>();
+        dfs(0, -1, vis, adj, tin, low, bridges);
+        return bridges;
     }
 }
-//     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        
-//     }
-// }
