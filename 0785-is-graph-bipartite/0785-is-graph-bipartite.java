956e6b6521a759ed639977a8426fd4c41c963871
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-05-19 12:08:14 +0530

    Time: 2 ms (35.68%), Space: 44.6 MB (6.63%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isBipartite(int[][] g) {
+         int[] colors = new int[g.length];
+        for (int i = 0; i < g.length; i++)
+            if (colors[i] == 0) {
+                Queue<Integer> q = new LinkedList<>();
+                q.add(i);
+                colors[i] = 1;
+                while (!q.isEmpty()) {
+                    Integer node = q.poll();
+                    for (int adjacent : g[node])
+                        if (colors[adjacent] == colors[node])
+                            return false;
+                        else if (colors[adjacent] == 0) {
+                            q.add(adjacent);
+                            colors[adjacent] = -colors[node];
+                        }
+                }
+            }
+        return true;
+    }
+}
