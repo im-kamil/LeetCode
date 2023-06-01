b20e9deb21c5ab536019e1241e2ef40c01528f4b
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-06-01 10:34:43 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.java
+++ Topological sort - GFG/topological-sort.java
@@ -68,11 +68,12 @@ class Solution
                 dfs(it, vis, st, adj);
         }
         st.push(node);
-    }
-
-    // Function to return list containing vertices in Topological order.
-    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
-        int vis[] = new int[V];
+            }
+    //Function to return list containing vertices in Topological order. 
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        // add your code here
+         int vis[] = new int[V];
         Stack<Integer> st = new Stack<Integer>();
         for (int i = 0; i < V; i++) {
             if (vis[i] == 0) {
@@ -87,5 +88,5 @@ class Solution
             st.pop();
         }
         return ans;
-}
+    }
 }
