227d7bb032ed93d11975b62c2cfb20c5f7bbd1b7
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-06-06 00:16:15 +0530

    Time: 1 ms (73.12%), Space: 40.8 MB (74.25%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,33 +1,34 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int length1 = word1.length(), length2 = word2.length();
+//         int length1 = word1.length(), length2 = word2.length();
         
-        char[] resultArr = new char[length1 + length2];
+//         char[] resultArr = new char[length1 + length2];
         
-        // Two pointers are maintained which will be used to 
-        // fetch the characters in alternating order.       
-        int pointer1 = 0, pointer2 = 0, resultPointer = 0;
+//         // Two pointers are maintained which will be used to 
+//         // fetch the characters in alternating order.       
+//         int pointer1 = 0, pointer2 = 0, resultPointer = 0;
         
-        //When either of the word's traversal is completed then loop
-        //will only consider the other word.
-        while (pointer1 < length1 || pointer2 < length2) {
-            if (pointer1 < length1) {
-                resultArr[resultPointer++] = word1.charAt(pointer1++);
-            }
-            if (pointer2 < length2) {
-                resultArr[resultPointer++] = word2.charAt(pointer2++);
-            }
+//         //When either of the word's traversal is completed then loop
+//         //will only consider the other word.
+//         while (pointer1 < length1 || pointer2 < length2) {
+//             if (pointer1 < length1) {
+//                 resultArr[resultPointer++] = word1.charAt(pointer1++);
+//             }
+//             if (pointer2 < length2) {
+//                 resultArr[resultPointer++] = word2.charAt(pointer2++);
+//             }
+//         }
+//         // return new String(resultArr);
+//         return new String(resultArr);
+        StringBuilder  sb = new StringBuilder();
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int i =0;
+        while(i<n1 || i<n2){
+            if(i<n1)  sb.append(word1.charAt(i));
+            if(i<n2) sb.append(word2.charAt(i));
+            i++;
         }
-        // return new String(resultArr);
-        return new String(resultArr);
-        // StringBuilder  sb = new StringBuilder();
-        // int n1 = word1.length();
-        // int n2 = word2.length();
-        // int i =0;
-        // while(i<n1 || i<n2){
-        //     if(i<n1)  sb.append(word1.charAt(i));
-        //     if(i<n2) sb.append(word2.charAt(i));
-        // }
-        // return sb.toString();
+        return sb.toString();
     }
 }
