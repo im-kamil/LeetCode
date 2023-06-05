0f7660e8f73bc1196835731294d0489bee778621
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-06-06 00:15:25 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (93.69%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -20,5 +20,14 @@ class Solution {
         }
         // return new String(resultArr);
         return new String(resultArr);
+        // StringBuilder  sb = new StringBuilder();
+        // int n1 = word1.length();
+        // int n2 = word2.length();
+        // int i =0;
+        // while(i<n1 || i<n2){
+        //     if(i<n1)  sb.append(word1.charAt(i));
+        //     if(i<n2) sb.append(word2.charAt(i));
+        // }
+        // return sb.toString();
     }
 }
