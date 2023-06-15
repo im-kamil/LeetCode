fb8a24e317651726c0a081c2af426f501cfe007b
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-06-15 09:56:34 +0530

    Added solution - LeetHub

--- Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.java
+++ Longest Palindrome in a String - GFG/longest-palindrome-in-a-string.java
@@ -24,49 +24,45 @@ class GFG
 
 //User function Template for Java
 
-class Solution{
-   int[][] dp;
-     String longestPalin(String s){
-        // code here
-        int n=s.length();
-        dp=new int[n+1][n+1];
-        for(int i=0;i<=n;i++){
-            Arrays.fill(dp[i],-1);
-        }
-        int temp=function(s,0,s.length()-1);
-        // System.out.println(temp);
-        if(temp==1) return ""+s.charAt(0);
-        
-        String t=ans;
-        ans="";
-        return t;
-    }
-     boolean isPalindrome(String s,int i,int j){
-        while(i<j){
-            if(s.charAt(i)!=s.charAt(j)){
-                return false;
+class Solution{ 
+    public static void fun(String S, int [][] dp){
+        int n = S.length();
+        int gap=0;
+        while(gap < n){
+            int i=0, j= gap;
+            while(i<n && j<n){
+                if(gap == 0) dp[i][j] = 1;
+                else if(gap == 1){
+                    if(S.charAt(i) == S.charAt(j)) dp[i][j] = 1;
+                    else dp[i][j] = 0;
+                }else{
+                    if(S.charAt(i) == S.charAt(j) && dp[i+1][j-1] == 1) dp[i][j] = 1;
+                    else dp[i][j] = 0;
+                }
+                i++;
+                j++;
             }
-            i++;
-            j--;
+            gap++;
         }
-        return true;
     }
-    
-     String ans="";
-     
-     int function(String s,int i,int j){
-        if(i>j) return 0;
-        if(i==j) return 1;
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        if(isPalindrome(s,i,j)){
-            if(ans.length()<j-i+1){
-                ans=s.substring(i,j+1);
-            }
-            return dp[i][j]=j-i+1;
+    static String longestPalin(String S){
+        int n = S.length();
+        int dp[][] = new int[n][n];
+        String st="";
+        int max=0;
+        fun(S,dp);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(dp[i][j] == 1) {
+                    if(j-i+1 > max) {
+                        max = j-i+1;
+                        st= S.substring(i,j+1);
+                    }
+                    
+                }
+            }   
         }
+        return st;
         
-        return dp[i][j]=Math.max(function(s,i,j-1),function(s,i+1,j));
     }
 }
