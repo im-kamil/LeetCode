a06a40c695902489d374c84b87402f3362824f5a
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-07-21 01:08:04 +0530

    Time: 8 ms (82.31%), Space: 44 MB (77.79%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,12 +1,47 @@
 class Solution {
-    public int[] asteroidCollision(int[] a) {
-         LinkedList<Integer> s = new LinkedList<>(); // use LinkedList to simulate stack so that we don't need to reverse at end.
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] > 0 || s.isEmpty() || s.getLast() < 0)
-                s.add(a[i]);
-            else if (s.getLast() <= -a[i])
-                if (s.pollLast() < -a[i]) i--;
+    public int[] asteroidCollision(int[] asteroids) {
+        //  LinkedList<Integer> s = new LinkedList<>(); // use LinkedList to simulate stack so that we don't need to reverse at end.
+        // for (int i = 0; i < a.length; i++) {
+        //     if (a[i] > 0 || s.isEmpty() || s.getLast() < 0)
+        //         s.add(a[i]);
+        //     else if (s.getLast() <= -a[i])
+        //         if (s.pollLast() < -a[i]) i--;
+        // }
+        // return s.stream().mapToInt(i->i).toArray();
+         Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<asteroids.length;i++){
+            //if stack is empty or a positve value arrives then perform blind insertion
+            if(stack.isEmpty() || asteroids[i]>0){
+                stack.push(asteroids[i]);
+            } else {
+                while(!stack.isEmpty()){
+                    int top = stack.peek();
+                    if(top<0){
+                        stack.push(asteroids[i]);
+                        break;
+                    } 
+                    int modVal = Math.abs(asteroids[i]);
+                        if(modVal == top){
+                            stack.pop();
+                            break;
+                        } else if(modVal < top){
+                            break;
+                        } else {
+                            stack.pop();
+                            if(stack.isEmpty()){
+                                stack.push(asteroids[i]);
+                                break;
+                            }
+                        }
+                    
+                }
+            }
         }
-        return s.stream().mapToInt(i->i).toArray();
+        int len = stack.size();
+        int ansArray[] = new int[len];
+        for(int i=len-1;i>=0;i--){
+            ansArray[i] = stack.pop();
+        }
+        return ansArray;
     }
 }
