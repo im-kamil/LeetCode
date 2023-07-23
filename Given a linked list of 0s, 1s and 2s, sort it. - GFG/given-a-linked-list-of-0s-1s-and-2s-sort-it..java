bcf910ee4d1850ee2d1771d8d5892153d3bf4a71
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-07-23 12:35:23 +0530

    Added solution - LeetHub

--- Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..java
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..java
@@ -18,28 +18,31 @@ class Node
 
 class Driverclass
 {
-    public static void main (String[] args) 
+    public static void main (String[] args) throws IOException
     {
-        Scanner sc= new Scanner(System.in);
-        int t = sc.nextInt();
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
         
         while(t-- > 0)
         {
-            int n = sc.nextInt();
-            Node head = new Node(sc.nextInt());
+            int n =Integer.parseInt(in.readLine().trim());
+            String s[] = in.readLine().trim().split(" ");
+            Node head = new Node(Integer.parseInt(s[0]));
             Node tail = head;
-            while(n-- > 1){
-		        tail.next = new Node(sc.nextInt());
-		        tail = tail.next;
-		    }
-		   
-		      head = new Solution().segregate(head);
-		     printList(head);
-		    System.out.println();
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(s[i]));
+                tail = tail.next;
+            }
+
+            head = new Solution().segregate(head);
+            printList(head, out);
+            out.println();
         }
+        out.close();
     }
     
-    public static void printList(Node head)
+    public static void printList(Node head,PrintWriter out)
     {
         if(head == null)
            return;
@@ -47,7 +50,7 @@ class Driverclass
         Node temp = head;
         while(temp != null)
         {
-            System.out.print(temp.data + " ");
+            out.print(temp.data + " ");
             temp = temp.next;
         }
     }
@@ -81,108 +84,44 @@ class Solution
     static Node segregate(Node head)
     {
         // add your code here
-//           if(head==null || head.next==null)return head;
-
+         ArrayList<Node> a = new ArrayList<>();
         
-
-//         Node zero=new Node(-1);
-
-//         Node pz=zero;
-
+        Node curr = head;
         
-
-//          Node one=new Node(-1);
-
-//         Node po=one;
-
-        
-
-//          Node two=new Node(-1);
-
-//         Node pt=two;
-
-        
-
-//         Node curr=head;
-
-        
-
-//         while(curr!=null){
-
-//             if(curr.data==0){
-
-//                 pz.next=curr;
-
-//                 pz=pz.next;
-
-//             }
-
-//             else if(curr.data==1){
-
-//                  po.next=curr;
-
-//                 po=po.next;
-
-//             }
-
-//             else{
-
-//                  pt.next=curr;
-
-//                 pt=pt.next;
-
-//             }
-
+        while(curr!= null){
             
-
-//             curr=curr.next;
-
-//         }
-
+            a.add(curr);    
+            curr = curr.next;
+        }
         
-
-//         po.next=two.next;
-
-//         pz.next=one.next;
-
-//         pt.next=null;
-
+        int l =0;
+        int m = 0;
+        int r = a.size()-1;
         
-
-//         return zero.next;
-
-    
-//     }
-// }
-  Node temp = head;
-
-        ArrayList<Integer> list = new ArrayList<Integer>();
-
-        while(temp != null){
-
-            list.add(temp.data);
-
-            temp = temp.next;
+        while(m<=r){
+            
+            if(a.get(m).data==0){
+                
+                int temp = a.get(m).data;
+                a.get(m).data = a.get(l).data;
+                a.get(l).data = temp;
+                l++;
+                m++;
+            }
+            else if(a.get(m).data==1){
+                m++;
+            }
+            else{
+                int temp = a.get(m).data;
+                a.get(m).data = a.get(r).data;
+                a.get(r).data = temp;
+                r--;
+                
+            }
 
         }
-
-        Collections.sort(list);
-
-        temp = head;
-
-        int i = 0;
-
-        while(temp != null){
-
-            temp.data = list.get(i);
-
-            temp = temp.next;
-
-            i++;
-
-        }
-
-        return head;
-
+  return head;
+    }
 }
-}
+
+
