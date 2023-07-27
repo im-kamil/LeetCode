433d7539fbb33b3a37bc307fec6f48016227167b
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-07-27 12:02:46 +0530

    Time: 27 ms (40.00%), Space: 57.3 MB (37.27%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long maxRunTime(int n, int[] A) {
+         Arrays.sort(A);
+        long sum = 0;
+        for (int a: A)
+            sum += a;
+        int k = 0, na = A.length;
+        while (A[na - 1 - k] > sum / (n - k))
+            sum -= A[na - 1 - k++];
+        return sum / (n - k);
+    }
+}
