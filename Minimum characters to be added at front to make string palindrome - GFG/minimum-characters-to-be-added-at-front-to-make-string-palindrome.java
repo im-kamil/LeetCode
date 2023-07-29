d7fd0c8b037296b82ebf50523ab93fb8fc961967
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-07-29 15:11:36 +0530

    Added solution - LeetHub

---
+++ Minimum characters to be added at front to make string palindrome - GFG/minimum-characters-to-be-added-at-front-to-make-string-palindrome.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution {
+    public static int minChar(String s) {
+       //Write your code here
+       int i =0;
+       int j = s.length()-1 , trim = j, res=0;
+       while(i<j){
+           if(s.charAt(i)==s.charAt(j)) {
+               i++;
+               j--;
+           }else{
+               res++;
+               i=0;
+               j = --trim;
+           }
+       }
+       return res;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	    var sc = new FastReader();
+	    int test = sc.nextInt();
+	    while(test-->0){
+    		String s = sc.nextString();
+    		System.out.println(Solution.minChar(s));
+	    }
+	}
+    
+    public static class FastReader
+	{
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader()
+		{
+			br = new BufferedReader(new InputStreamReader(System.in));
+			st = new StringTokenizer("");
+		}
+
+		private void read() throws IOException
+		{
+			st = new StringTokenizer(br.readLine());
+		}
+
+		public String nextString() throws IOException
+		{
+			while(!st.hasMoreTokens())
+				read();
+			return st.nextToken();
+		}
+
+		public int nextInt() throws IOException
+		{
+			return Integer.parseInt(nextString());
+		}
+
+		public long nextLong() throws IOException
+		{
+			return Long.parseLong(nextString());
+		}
+	}
+}
+// } Driver Code Ends
