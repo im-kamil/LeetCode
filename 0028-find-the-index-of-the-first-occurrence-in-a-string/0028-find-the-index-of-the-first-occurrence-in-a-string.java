97d42614d540eb03b5d49f89741169b02e29f051
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-07-29 14:53:11 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (42.39%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+//         if(needle.length() == 0) return 0;
+        
+//         char[] hay = haystack.toCharArray();
+//         char[] need = needle.toCharArray();
+//         int valueLen = hay.length;
+//         int strLen = need.length;
+        
+//         int max = valueLen - strLen;
+        
+//         for(int i=0; i <= max; i++) {
+//             // Find the first character
+//             if(hay[i] != need[0]) {
+//                 while(++i <= max && hay[i] != need[0]);
+//             }
+            
+//             if(i <= max) {
+//                 int k = i+1;
+//                 int foundLength = i+strLen;
+//                 for(int j=1; k<foundLength && needle.charAt(j) == hay[k]; j++, k++);
+//                 if(k==foundLength) {
+//                     return i;
+//                 }
+//             }
+//         }
+//         return -1;
+//         fot(int i=0; i<haystack.length()-needle.length()+1;i++){
+//             if(haystack.substring(i, needle.length()+1.equals(needle)) return i;
+               
+//         }
+//     }
+//                return -1;
+          if (haystack.contains(needle)) {
+            return haystack.indexOf(needle);
+        } else {
+            return -1;
+          }
+               }
+}
