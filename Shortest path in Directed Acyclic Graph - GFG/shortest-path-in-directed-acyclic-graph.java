682cc2b9f9d742cceef0bf2ae600161640bdd960
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-08-03 01:35:24 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.java
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.java
@@ -29,76 +29,47 @@ class Main {
 
 
 //User function Template for Java
-class Pair {
-  int first, second;
-  Pair(int _first, int _second) {
-    this.first = _first;
-    this.second = _second;
-  }
-}
 class Solution {
-    private void topoSort(int node, ArrayList < ArrayList < Pair >> adj,
-    int vis[], Stack < Integer > st) {
-    //This is the function to implement Topological sort. 
 
-    vis[node] = 1;
-    for (int i = 0; i < adj.get(node).size(); i++) {
-      int v = adj.get(node).get(i).first;
-      if (vis[v] == 0) {
-        topoSort(v, adj, vis, st);
-      }
-    }
-    st.add(node);
-  }
-  public int[] shortestPath(int N, int M, int[][] edges) {
-    ArrayList < ArrayList < Pair >> adj = new ArrayList < > ();
-    for (int i = 0; i < N; i++) {
-      ArrayList < Pair > temp = new ArrayList < Pair > ();
-      adj.add(temp);
-    }
-    //We create a graph first in the form of an adjacency list.
+	public int[] shortestPath(int N,int M, int[][] edges) {
+		//Code here
+		int dis[]=new int [N];
+		for(int i=0;i<N;i++){
+		    dis[i]=Integer.MAX_VALUE;
+		}
+		dis[0]=0;
+		Stack <Integer> st=new Stack<>();
+		boolean vis[]=new boolean[N];
+		for(int i=0;i<N;i++){
+		    if(!vis[i])
+		    topsort(edges,vis,i,st);
+		}
+	
+		while(!st.isEmpty()){
+		    int k=st.pop();
+		    for(int i=0;i<M;i++){
+		        if(edges[i][0]==k && dis[k]!=Integer.MAX_VALUE){
+		            if(dis[edges[i][1]]>(dis[k]+edges[i][2]))
+		            dis[edges[i][1]]=dis[k]+edges[i][2];
+		        }
+		    }
+		}
+		for(int i=0;i<N;i++){
+		    if(dis[i]==Integer.MAX_VALUE)
+		    dis[i]=-1;
+		}
+		return dis;
+	}
+	
+	public void topsort(int edges[][],boolean vis[],int s,Stack<Integer> st){
+	    vis[s]=true;
+	    for(int i=0;i<edges.length;i++){
+	        if(edges[i][0]==s){
+	            if(!vis[edges[i][1]])
+	            topsort(edges,vis,edges[i][1],st);
+	        }
+	    }
+	    st.push(s);
 
-    for (int i = 0; i < M; i++) {
-      int u = edges[i][0];
-      int v = edges[i][1];
-      int wt = edges[i][2];
-      adj.get(u).add(new Pair(v, wt));
-    }
-    int vis[] = new int[N];
-    //Now, we perform topo sort using DFS technique 
-    //and store the result in the stack st.
-
-    Stack < Integer > st = new Stack < > ();
-    for (int i = 0; i < N; i++) {
-      if (vis[i] == 0) {
-        topoSort(i, adj, vis, st);
-      }
-    }
-    //Further, we declare a vector ‘dist’ in which we update the value of the nodes’
-    //distance from the source vertex after relaxation of a particular node.
-    int dist[] = new int[N];
-    for (int i = 0; i < N; i++) {
-      dist[i] = (int)(1e9);
-    }
-
-    dist[0] = 0;
-    while (!st.isEmpty()) {
-      int node = st.peek();
-      st.pop();
-
-      for (int i = 0; i < adj.get(node).size(); i++) {
-        int v = adj.get(node).get(i).first;
-        int wt = adj.get(node).get(i).second;
-
-        if (dist[node] + wt < dist[v]) {
-          dist[v] = wt + dist[node];
-        }
-      }
-    }
-
-    for (int i = 0; i < N; i++) {
-      if (dist[i] == 1e9) dist[i] = -1;
-    }
-    return dist;
 	}
 }
