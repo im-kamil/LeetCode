7f0a6feb1b96685f90fd2ba6c40b3792ecdfbc8b
Md Kamil Nasar <94951157+im-kamil@users.noreply.github.com>
2023-09-15 12:59:40 +0530

    Added solution - LeetHub

---
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.java
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String input_line[] = in.readLine().trim().split("\\s+");
+            int arr[] = new int[N];
+            for(int i = 0;i < N;i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            int x = ob.equalPartition(N, arr);
+            if(x == 1)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static int equalPartition(int N, int arr[])
+    {
+        // code here
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+
+        if (sum % 2 != 0) {
+            return 0;
+        }
+
+        int targetSum = sum / 2;
+
+        boolean[][] dp = new boolean[N + 1][targetSum + 1];
+
+        for (int i = 0; i <= N; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= targetSum; j++) {
+                if (arr[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+                }
+            }
+        }
+
+        return dp[N][targetSum] ? 1 : 0;
+    }
+}
